5328c4171433ee42f613c6855f25952121fc9356
Jorrodlop <jorrod03@ucm.es>
2025-08-19 14:35:06 +0200

    Integrados nuevos atributos

--- Clasificador_usar.py
+++ Clasificador_usar.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn.functional as F
 import numpy
-from MLP import MLP, cargar_MLP, guardar_MLP, nombre_a_func
+from MLP import MLP, nombre_a_func
+from Autoencoder import Autoencoder 
+from Clasificador import Clasificador 
+from Guardar_Cargar import guardar_modelo, cargar_modelo
 from Procesar_datos import procesar_datos
-from Autoencoder import Autoencoder, guardar_autoencoder, cargar_autoencoder
-from Clasificador import Clasificador, guardar_classificador, cargar_classificador
 from Visual import visual
 
 
@@ -27,8 +28,10 @@ def vector_a_clase(xs):
     return xs.argmax().item()
 
 ## NOMBRE Clasificador ##
-archivo_clasificador = r"redes_disponibles\intento1_Clasificador.json"
+archivo_clasificador = r"redes_disponibles\clasificador_prueba_bueno.json"
+ver_descripcion = True
 ver_solo_decision = True # Si es True veremos SOLO que clase asigna a los datos el clasificador. En caso contrario veremos las "Probabilidades".
+
 
 ## DATOS a clasificar / TEST a evaluar ##
 modo_evaluacion = True
@@ -36,26 +39,29 @@ modo_evaluacion = True
 xs_a_clasificar = None
 ys_para_evaluar = None
 
-_,_,_,xs_a_clasificar,ys_para_evaluar,etiquetas_test = procesar_datos(archivo_set_train="datasets/nba_pergame_24_full.csv",
-                                                                      archivo_set_test="datasets/nba_pergame_24_full.csv",
-                                                                      modo_autoencoder=False,
-                                                                      modo_columnas="solo_volumen",
-                                                                      modo_targets="pos",
-                                                                      modo_etiquetado="posicion",
-                                                                      normalizar_datos=True,
-                                                                      modo_normalizacion="zscore",
-                                                                      umbral_partidos=5,
-                                                                      umbral_minutos=5,
-                                                                      umbral_en_test=True,
-                                                                      hay_fila_total_entrenamiento=False,
-                                                                      hay_fila_total_test=True)
+archivo_entrenamiento = "datasets/nba_pergame_24_full.csv"
+archivo_test = r"datasets/nba_pergame_24_full.csv"
+_, _, _, xs_a_clasificar, ys_para_evaluar, etiquetas_test = procesar_datos(archivo_set_train=archivo_entrenamiento,
+                                                                                    archivo_set_test=archivo_test,
+                                                                                    modo_autoencoder=False,
+                                                                                    modo_columnas="solo_volumen",
+                                                                                    modo_targets="pos",
+                                                                                    modo_etiquetado="posicion",
+                                                                                    normalizar_datos=True,
+                                                                                    modo_normalizacion="zscore",
+                                                                                    umbral_partidos=20,
+                                                                                    umbral_minutos=15,
+                                                                                    umbral_en_test=True,
+                                                                                    hay_fila_total_entrenamiento=False,
+                                                                                    hay_fila_total_test=True)
 
 ## OPCIONES visualizacion ##
 ver_repr_latente = True
 
 dim_repr = 3 
 modos_redd_dim = ["pca", "tsne", "umap"]
-modo_redd_dim = "tsne"
+modo_redd_dim = "pca"
+visualizar_todos = True
 titulo_grafico = "Titulo"
 
 
@@ -66,7 +72,9 @@ if __name__=="__main__":
     
     # Cargamos Clasificador
     print(f"Se va a usar el Clasificador: {archivo_clasificador}.\n")
-    clasificador = cargar_classificador(archivo_clasificador)
+    clasificador = cargar_modelo(archivo_clasificador)
+    if ver_descripcion:
+        print(f"La descripcion de este modelo nos dice: \n'{clasificador.description}'")
 
     if modo_evaluacion :
         print("\nEstamos en modo Evaluacion.\n")
@@ -75,14 +83,14 @@ if __name__=="__main__":
     # Pasamos datos por el clasificador y extraemos su clase
     datos_clasificados = clasificador(xs_a_clasificar).detach().numpy()
     clases_datos = [vector_a_clase(xs) for xs in datos_clasificados]
+
     # Mostramos resultados del clasificador
     print(f"El clasificador asigna a los datos las siguientes clases:\n{clases_datos}\n")
     if not ver_solo_decision:
         print(f"Con unas probabilidades asignadas:\n {datos_clasificados}\n")
     
-
+    #  Comparacion con el test
     if modo_evaluacion:
-        # Comparamos resultado con el test
         cont_correctos = 0
         for i in range(len(clases_test)):
             if clases_test[i] == clases_datos[i]:
@@ -93,10 +101,26 @@ if __name__=="__main__":
         print(f"El modelo {archivo_clasificador} ha acertado {cont_correctos} / {len(clases_test)}.")
         print(f"Precisión en test: {accuracy*100:.2f}%\n")
 
-    
+    # Visualización
     if ver_repr_latente:
-        visual(archivo_clasificador,xs_a_clasificar,dim_repr,clases_datos,titulo_grafico,modo_redd_dim)
-        visual(archivo_clasificador,xs_a_clasificar,dim_repr,clases_test,"Clases test",modo_redd_dim)
+        if visualizar_todos:
+            visual(archivo_clasificador,xs_a_clasificar,dim_repr,clases_datos,titulo_grafico,"pca")
+            if modo_evaluacion:
+                visual(archivo_clasificador,xs_a_clasificar,dim_repr,clases_test,"Clases test","pca")
+            
+            visual(archivo_clasificador,xs_a_clasificar,dim_repr,clases_datos,titulo_grafico,"tsne")
+            if modo_evaluacion:
+                visual(archivo_clasificador,xs_a_clasificar,dim_repr,clases_test,"Clases test","tsne")
+            
+            visual(archivo_clasificador,xs_a_clasificar,dim_repr,clases_datos,titulo_grafico,"umap")
+            if modo_evaluacion:
+                visual(archivo_clasificador,xs_a_clasificar,dim_repr,clases_test,"Clases test","umap")
+        
+
+        else:
+            visual(archivo_clasificador,xs_a_clasificar,dim_repr,clases_datos,titulo_grafico,modo_redd_dim)
+            if modo_evaluacion:
+                visual(archivo_clasificador,xs_a_clasificar,dim_repr,clases_test,"Clases test",modo_redd_dim)
 
 
     
